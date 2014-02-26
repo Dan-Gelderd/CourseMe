b9dd1dfc04281d180d0daa4b05f0af3c7f82eee3
BigPoppaG <BigPoppaG@server.fake>
2014-02-26 13:25:50 +0000

    Logins broken with session error.
    Following Flask Mega Tutorial. Module structure not quite working the
    same, needed to put everything in the app folder. Getting object in
    session 1 (this is 2) errors with the user login system.

--- app/app.py
+++ app/app.py
@@ -1,26 +1,89 @@
-#!/usr/bin/env python
+#!flask/bin/python
 
-from flask import Flask, render_template, request
+import flask
+from flask import g, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
+
+import os
+from flask.ext.login import LoginManager, login_user, logout_user, current_user, login_required
+from flask.ext.openid import OpenID
+from config import basedir
+
+import forms
 import datamodel
 import database
-import database_def
-import json
+import datetime
 
-app = Flask(__name__, static_folder="../static", static_url_path="/static")
+app = flask.Flask(__name__,
+            static_folder="../static",
+            static_url_path="/static")
+
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+import models
+db.create_all()
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('microblog startup')
 
 
-def search_objective(objective):
-    return {'lectures':[2, 3, 4], 'exercises':[2, 3, 4], 'tools':[2, 3, 4]}
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
 
-def alternative_modules(module, user):
-    alternative_modules={}
-    for objective in module.objectives:
-        alternative_modules[objective]=search_objectives(objective)
-    return alternative_modules
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
+
+@lm.user_loader
+def load_user(id):
+    return models.User.query.get(int(id))
+
+
+@app.before_request
+def before_request():
+    g.user = current_user
+    if g.user.is_authenticated():
+        g.user.last_seen = datetime.datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
+
 
 @app.route('/')
+@app.route('/index')
+#@login_required
 def index():
-    return 'Hello World!'
+    user = g.user
+    posts = [
+        { 
+            'author': { 'nickname': 'John' }, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': { 'nickname': 'Susan' }, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+
+    return flask.render_template('index.html',
+        user = user,
+        posts = posts)
 
 @app.route('/module/<name>')
 def module(name):
@@ -35,7 +98,7 @@ def module(name):
     authormodule = datamodel.UserModule.find(module.author, module)
     #tutormodule = datamodel.UserModule.find(module.author, module)    
 
-    return render_template('module.html',
+    return flask.render_template('module.html',
                            module=module,
                            usermodule=usermodule,
                            authormodule=authormodule,
@@ -57,6 +120,7 @@ def starclick(name):
     
     return usermodule.as_json()
 
+
 @app.route('/vote/<name>')
 def voteclick(name):
     
@@ -67,7 +131,7 @@ def voteclick(name):
     except KeyError:
         usermodule = datamodel.UserModule(user, module)
     
-    newVote = int(request.args.get("vote"))
+    newVote = int(flask.request.args.get("vote"))
     module.votes = module.votes - usermodule.vote + newVote       #DJG - Almost certainly a better way
     usermodule.vote = newVote
         
@@ -76,10 +140,71 @@ def voteclick(name):
     
     return ""   #DJG - What is best return value when I don't care about the return result? Only thing I found that worked
 
-@app.route('/objectives')
+
+@app.route('/objectives', methods = ['GET', 'POST'])
 def objectives():
-    return ""
-    
+    objectives = models.Objective.query.all()
+    form = forms.AddObjective()
+    if form.validate_on_submit():
+        objective = models.Objective(name=form.objective_name.data)
+        db.session.add(objective)
+        db.session.commit()
+    return flask.render_template('objectives.html',
+                                 form=form,
+                                 objectives=objectives)
+
+
+@app.route('/objectivedelete')
+def objectivedelete():
+    objective = models.Objective.query.filter_by(id = flask.request.args.get("objective_id")).first()
+    db.session.delete(objective)
+    db.commit
+    objectives = models.Objective.query.all()
+    return {"a": "A"}
+   
+   
+@app.route('/login', methods = ['GET', 'POST'])
+@oid.loginhandler
+def login():
+    if g.user is not None and g.user.is_authenticated():
+        return flask.redirect(url_for('index'))
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        flask.session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])        
+    return flask.render_template('login.html',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return flask.redirect(url_for('login'))
+    user = models.User.query.filter_by(email = resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+            nickname = models.User.make_unique_nickname(nickname)
+        user = models.User(nickname = nickname, email = resp.email, role = models.ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in flask.session:
+        remember_me = flask.session['remember_me']
+        flask.session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+    return flask.redirect(flask.request.args.get('next') or url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return flask.redirect(url_for('index'))
+
+
 def run_devserver():
     app.run(debug=True)
 
---
+++ app/models.py
@@ -0,0 +1,75 @@
+from app import db
+
+ROLE_USER = 0
+ROLE_ADMIN = 1
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    nickname = db.Column(db.String(64), index = True, unique = True)
+    email = db.Column(db.String(120), index = True, unique = True)
+    role = db.Column(db.SmallInteger, default = ROLE_USER)
+    last_seen =  db.Column(db.DateTime)
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
+    
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?d=mm&s=' + str(size)
+
+    @staticmethod
+    def make_unique_nickname(nickname):
+        if User.query.filter_by(nickname = nickname).first() == None:
+            return nickname
+        version = 2
+        while True:
+            new_nickname = nickname + str(version)
+            if User.query.filter_by(nickname = new_nickname).first() == None:
+                break
+            version += 1
+        return new_nickname
+
+objective_heirarchy = db.Table("objective_heirarchy",
+    db.Column("prerequisite_id", db.Integer, db.ForeignKey("objective.id")),
+    db.Column("followon_id", db.Integer, db.ForeignKey("objective.id"))
+)
+
+class Objective(db.Model):
+    #__tablename__ = 'objectives'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique = True)
+    prerequisites = db.relationship("Objective",
+                        secondary=objective_heirarchy,
+                        primaryjoin=(objective_heirarchy.c.followon_id==id),
+                        secondaryjoin=(objective_heirarchy.c.prerequisite_id==id),
+                        #backref = db.backref('followons', lazy = 'dynamic'), 
+                        lazy = 'dynamic')
+    
+
+    def require(self, objective):
+        if not self.is_required(objective):
+            self.prerequisites.append(objective)
+            return self
+
+    def unrequire(self, objective):
+        if self.is_required(objective):
+            self.prerequisites.remove(objective)
+            return self
+
+    def is_required(self, objective):
+        return self.prerequisites.filter(objective_heirarchy.c.prerequisite_id == objective.id).count() > 0
+
