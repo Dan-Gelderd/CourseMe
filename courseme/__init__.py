bd00d272ad04a4b40401c629031726073a48f282
BigPoppaG <BigPoppaG@server.fake>
2014-05-11 22:00:10 +0100

    First mac commit
    requirements and gitignore

--- courseme/__init__.py
+++ courseme/__init__.py
@@ -24,15 +24,15 @@ lectures = UploadSet('lectures', extensions=('mp4'))
 configure_uploads(app, (lectures))
 patch_request_class(app, 8 * 1024 * 1024)        # 16 megabytes
 
-if not app.debug:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
-    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('microblog startup')
+# if not app.debug:
+#     import logging
+#     from logging.handlers import RotatingFileHandler
+#     file_handler = RotatingFileHandler('tmp/courseme.log', 'a', 1 * 1024 * 1024, 10)
+#     file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+#     app.logger.setLevel(logging.INFO)
+#     file_handler.setLevel(logging.INFO)
+#     app.logger.addHandler(file_handler)
+#     app.logger.info('microblog startup')
 
 def hash_string(string):
     salted_hash = string + app.config['SECRET_KEY']
