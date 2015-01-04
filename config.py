6183d42e967598cd2e77d02a05c675e24f1d155b
BigPoppaG <BigPoppaG@server.fake>
2015-01-04 22:49:44 +0000

    Starting to use flask-mail

--- config.py
+++ config.py
@@ -23,6 +23,11 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'courseme-dev.sqlite')
 
 
--- run.py
+++ run.py
@@ -2,17 +2,21 @@
 from courseme import app, db, models
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask.ext.mail import Mail
 
 manager = Manager(app)
 
 migrate = Migrate(app, db)
 
+mail = Mail(app)
+
 
 def make_shell_context():
     return dict(app=app, db=db, User=models.User, Module=models.Module, Objective=models.Objective)
+
+
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
-
 
 if __name__ == '__main__':
     manager.run()
