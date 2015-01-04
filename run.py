78208de5b9c64e3ef91717f91700a950106263fa
BigPoppaG <BigPoppaG@server.fake>
2015-01-04 12:41:53 +0000

    Include flask-script manager

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 #!flask/bin/python
-from courseme import app
-app.run(debug = True)
+from courseme import app, db
+from flask.ext.script import Manager, Shell
+
+manager = Manager(app)
+
+def make_shell_context():
+    return dict(app=app, db=db)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+if __name__ == '__main__':
+    manager.run()
