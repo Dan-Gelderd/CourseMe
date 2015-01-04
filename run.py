a76d9f19f55201f1cf360e08b35ea489117c1edc
BigPoppaG <BigPoppaG@server.fake>
2015-01-04 20:40:02 +0000

    Additions to Flask-Script shell context

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 #!flask/bin/python
-from courseme import app, db
+from courseme import app, db, models
 from flask.ext.script import Manager, Shell
 
 manager = Manager(app)
 
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, User=models.User, Module=models.Module, Objective=models.Objective)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
