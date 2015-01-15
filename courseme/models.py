122a5236a6987a1b25396237461e3317fb2840f0
BigPoppaG <BigPoppaG@server.fake>
2015-01-15 18:16:16 +0000

    Account confirmation unit tests working

--- courseme/models.py
+++ courseme/models.py
@@ -143,7 +143,7 @@ class User(db.Model):
     def confirm(self, token):
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
-            data = s.load(token)
+            data = s.loads(token)
         except:
             return False
         if data.get('confirm') != self.id:
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -4,6 +4,17 @@ from courseme.models import User
 from courseme import create_app, db
 
 class UserModelTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
     def test_password_setter(self):
         u = User(password='cat')
         self.assertTrue(u.password_hash is not None)
